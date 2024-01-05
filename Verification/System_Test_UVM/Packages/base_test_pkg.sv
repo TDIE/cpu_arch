d9e5f8d1f3533cc7a274b27a86532230158ca1bd
Tom <TDIE@users.noreply.github.com>
2024-01-04 17:25:10 -0800

    Added UVM testbench packages.
    UVM testbench must be compiled at package level to prevent undefined variables/classes errors or cyclical reference errors.

---
+++ Verification/System_Test_UVM/Packages/base_test_pkg.sv
@@ -0,0 +1,12 @@
+//This package contains everything to run the UVM base test for the cpu
+//Author: Tom Diederen
+//December 2023
+//Part of Rudimentary Processor Design Project: https://github.com/TDIE/cpu_arch
+
+package base_test_pkg;
+    import uvm_pkg::*;
+    import cpu_agent_pkg::*;
+    import cpu_env_pkg::*;
+    `include "base_test.svh"
+    //`include "cpu_bus_seq.svh"
+endpackage: base_test_pkg
