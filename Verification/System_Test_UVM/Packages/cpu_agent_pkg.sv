ca34f7bf324436f27e398121399b5afd085e2981
Tom <TDIE@users.noreply.github.com>
2024-01-04 17:26:54 -0800

    Added CPU agent and CPU environment packages.
    UVM testbench must be compiled at package level to prevent undefined variable/class errors or cyclical reference errors.

---
+++ Verification/System_Test_UVM/Packages/cpu_agent_pkg.sv
@@ -0,0 +1,15 @@
+//This package contains everything related to the cpu_agent
+//Author: Tom Diederen
+//December 2023
+//Part of Rudimentary Processor Design Project: https://github.com/TDIE/cpu_arch
+
+package cpu_agent_pkg;
+    import uvm_pkg::*;
+    `include "uvm_macros.svh"
+    `include "cpu_agent.svh"
+    // `include "cpu_monitor.svh"
+    // `include "cpu_driver.svh"
+    // `include "cpu_mon_bus_seq_item.svh"
+    // `include "cpu_drv_bus_seq_item.svh"
+    // `incluce "cpu_agent_config"
+endpackage: cpu_agent_pkg
---
+++ Verification/System_Test_UVM/Packages/cpu_env_pkg.sv
@@ -0,0 +1,14 @@
+//This package contains everything related to the cpu_env
+//Author: Tom Diederen
+//December 2023
+//Part of Rudimentary Processor Design Project: https://github.com/TDIE/cpu_arch
+
+package cpu_env_pkg;
+    import uvm_pkg::*;
+    import cpu_agent_pkg::*;
+    `include "uvm_macros.svh"
+    `include "cpu_env.svh"
+    //`include "scoreboard.svh"
+    //`include "coverage_collector.svh"
+    //`include "cpu_env_config.svh"
+endpackage: cpu_env_pkg
